d032e8e02241fef960a1d9b5bb78520358870b53
koflazycat <lushoulei0552@163.com>
2023-12-06 00:48:56 +0800

    1. 方块突击增加room和player场景 2. 增加字体、纹理等资源

---
+++ SquareHit/src/main/scene/role/player/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var visuals: Node2D = $Visuals
+@onready var sprite_2d: Sprite2D = $Visuals/Sprite2D
+
+@export var speed: int = 100
+@export var acceleration: float = 5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_player.play("idle")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var direction: Vector2 = get_movement_vector()
+	accelerate_in_direction(direction)
+	move_and_slide()
+	
+	if direction != Vector2.ZERO:
+		animation_player.play("walk")
+	else:
+		animation_player.play("idle")
+	
+	var move_sign: int = sign(direction.x)
+	if move_sign != 0:
+		visuals.scale.x = move_sign
+
+
+func get_movement_vector() -> Vector2:
+	var x_movement: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement: float = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(x_movement, y_movement).normalized()
+
+
+func accelerate_in_direction(direction: Vector2) -> void:
+	var desired_velocity: Vector2 = direction * speed
+	velocity = velocity.lerp(desired_velocity, 1.0 - exp(-acceleration * get_process_delta_time()))
+

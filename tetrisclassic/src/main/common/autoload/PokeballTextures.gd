a7f90fc585628a74abf65c5c3d147b6b70418f09
koflazycat <lushoulei0552@163.com>
2024-06-04 16:00:26 +0800

    1. 初始化经典俄罗斯方块游戏

---
+++ tetrisclassic/src/main/common/autoload/PokeballTextures.gd
@@ -0,0 +1,21 @@
+extends Node
+
+const pokeball0 = preload("res://src/main/assets/textures/poke/poke0.png")
+const pokeball1 = preload("res://src/main/assets/textures/poke/poke1.png")
+const pokeball2 = preload("res://src/main/assets/textures/poke/poke2.png")
+const pokeball3 = preload("res://src/main/assets/textures/poke/poke3.png")
+const pokeball4 = preload("res://src/main/assets/textures/poke/poke4.png")
+const pokeball5 = preload("res://src/main/assets/textures/poke/poke5.png")
+const pokeball6 = preload("res://src/main/assets/textures/poke/poke6.png")
+const pokeball7 = preload("res://src/main/assets/textures/poke/poke7.png")
+
+func getTextureForColorIndex(index):
+	match (index):
+		(0): return pokeball0
+		(1): return pokeball1
+		(2): return pokeball2
+		(3): return pokeball3
+		(4): return pokeball4
+		(5): return pokeball5
+		(6): return pokeball6
+		(7): return pokeball7
---
+++ tetrisclassic/src/main/script/piece.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+var positionInGrid: Vector2
+var rotationState: int = 0
+var shape: Array

9e6889b427cc1dcf361dc7acf9ae0b1e4e8a31ad
koflazycat <lushoulei0552@163.com>
2024-06-05 12:30:08 +0800

    1. 增加方块自由下落逻辑

--- tetrisclassic/src/main/scene/level/main/grid.gd
+++ tetrisclassic/src/main/scene/level/main/grid.gd
@@ -9,17 +9,17 @@ const spriteSize: int = 32
 
 const BORDER_OFFSET: float = 10
 const Piece = preload("res://src/main/script/piece.gd")
-const darkMaterial = preload("res://src/main/assets/shaders/DarkMaterial.tres")
-#const DropParticle = preload("res://scn/DropParticle.tscn")
-#const ClearParticle = preload("res://scn/ClearParticle.tscn")
-#const HoldParticle = preload("res://scn/HoldParticle.tscn")
+const darkMaterial = preload("res://src/main/assets/shaders/dark_material.tres")
+const DropParticle = preload("res://src/main/scene/particles/drop_particle.tscn")
+const ClearParticle = preload("res://src/main/scene/particles/clear_particle.tscn")
+const HoldParticle = preload("res://src/main/scene/particles/hold_particle.tscn")
 
 var grid: Array[Array] = []
 var gridOffsetX: float
 var gridOffsetY: float
 var currentPiece: Piece
 
-var timer = 0
+var timer: float = 0
 var deltaSum = 0
 var clearedLines = 0
 var dasCounter = 0
@@ -28,7 +28,7 @@ var level = 1
 var score = 0
 var actions = 0
 var prevActions = 0
-var speed = 1
+var speed: float = 1
 var hasSwapped = false
 
 var currentBag: Array[Node2D]
@@ -56,6 +56,21 @@ func _physics_process(delta: float) -> void:
 	var sthHappened: bool = false
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+	
+	timer += delta
+	if timer >= speed:
+		if canPieceMoveDown():
+			movePieceInGrid(0,1)
+			actions = 0
+			sthHappened = true
+		else:
+			if $LockTimer.time_left == 0:
+				$LockTimer.start()
+		timer = 0
+
+	if (sthHappened):
+		drawGrid()
+		drawDroppingPoint()
 
 
 # 生成新的方块数组
@@ -156,10 +171,142 @@ func drawDroppingPoint():
 					add_child(circle)
 
 
-# 删除初始化在网格中的方块
+# 删除网格中当前的方块
 func deletePieceFromGrid() -> void:
 	#Delete old position
 	for x in currentPiece.shape.size():
 		for y in currentPiece.shape[0].size():
 			if currentPiece.shape[x][y] != 0:
 				grid[x + currentPiece.positionInGrid.x][y + currentPiece.positionInGrid.y] = 0
+
+
+# 是否可以向下移动一格
+func canPieceMoveDown() -> bool:
+	for i in range(0, currentPiece.shape.size()):
+		for j in range(currentPiece.shape[0].size() - 1, -1, -1):
+			if currentPiece.shape[i][j] != 0:
+				if currentPiece.positionInGrid.y + j + 1 >= gridHeight:
+					return false
+				if grid[currentPiece.positionInGrid.x + i][currentPiece.positionInGrid.y + j + 1] != 0:
+					return false
+				else: 
+					break
+	return true
+
+
+# 是否可以向右移动一格
+func canPieceMoveRight() -> bool:
+	for j in currentPiece.shape[0].size():
+		for i in range (currentPiece.shape.size() - 1, -1, -1):
+			if currentPiece.shape[i][j] != 0:
+				if currentPiece.positionInGrid.x + i + 1 >= gridWidth:
+					return false
+				if grid[currentPiece.positionInGrid.x + i +1][currentPiece.positionInGrid.y + j] != 0:
+					return false
+				else: 
+					break
+	return true
+
+
+# 是否可以向左移动一格
+func canPieceMoveLeft()  -> bool:
+	for j in currentPiece.shape[0].size():
+		for i in currentPiece.shape.size():
+			if currentPiece.shape[i][j] != 0:
+				if currentPiece.positionInGrid.x + i <= 0:
+					return false
+				if grid[currentPiece.positionInGrid.x + i -1][currentPiece.positionInGrid.y + j] != 0:
+					return false
+				else: 
+					break
+	return true
+
+
+# 移动方块
+func movePieceInGrid(xMovement: int, yMovement: int) -> void:
+	# 先删除方块
+	deletePieceFromGrid()
+	#Write new position
+	# 再在新位置重绘方块
+	for x in range(0, currentPiece.shape.size()):
+		for y in range(0, currentPiece.shape[0].size()):
+			if currentPiece.shape[x][y] != 0:
+				grid[x + xMovement + currentPiece.positionInGrid.x][y + yMovement + currentPiece.positionInGrid.y] = currentPiece.shape[x][y]
+	
+	currentPiece.positionInGrid.x = currentPiece.positionInGrid.x + xMovement
+	currentPiece.positionInGrid.y = currentPiece.positionInGrid.y + yMovement
+
+
+func afterDrop() -> void:
+	currentPiece = Piece.new()
+	checkAndClearFullLines()
+	if (checkGameOver()):
+		get_tree().quit()
+	spawnFromBag()
+	hasSwapped = false
+
+
+func checkAndClearFullLines() -> void:
+	var cleared: int = 0
+	for y in range(gridHeight):
+		var fullLine: bool = true
+		for x in range(gridWidth):
+			if grid[x][y] == 0:
+				fullLine = false
+				break;
+		if fullLine:
+			cleared += 1
+			#Clear line
+			for x in range(gridWidth):
+				grid[x][y] = 0
+			#Move everything down
+			for j in range(y, 0, -1):
+				for i in range(gridWidth):
+					if (grid[i][j] == 0) && (grid[i][j - 1] != 0):
+						grid[i][j] = grid[i][j-1]
+						grid[i][j-1] = 0
+			#Draw clear particle
+			var particle = ClearParticle.instantiate()
+			particle.position.x = gridOffsetX + spriteSize*gridWidth/2.0
+			var pixelPosy = (y)* spriteSize
+			particle.position.y = (pixelPosy) + gridOffsetY
+			particle.setBoxRange(spriteSize*gridWidth/2.0)
+			add_child(particle)
+			particle.emit()
+	#Scoring
+	if cleared != 0:
+		var newScore: int
+		# 同时清理行数越多，积分越高
+		match (cleared):
+			1: newScore = 100 * level
+			2: newScore = 300 * level
+			3: newScore = 500 * level
+			4: newScore = 800 * level
+		score += newScore
+		lines += cleared
+		$UI/Score/ScoreNumber.text = str(score)
+		$UI/Lines/LinesNumber.text = str(lines)
+		clearedLines += cleared
+		if (clearedLines >= 10):
+			clearedLines = clearedLines-10
+			level+=1
+			speed = pow(0.8-(level-1)*0.007,level-1)
+			$UI/Level/LevelNumber.text = str(level)
+
+
+func checkGameOver() -> bool:
+	for i in range (gridWidth):
+		if grid[i][vanishZone-1] != 0:
+			return true
+	return false
+
+
+func _on_lock_timer_timeout() -> void:
+	if (actions > infinityValue) || (prevActions == actions):
+		if !canPieceMoveDown():
+			afterDrop()
+			drawGrid()
+			drawDroppingPoint()
+	else:
+		$LockTimer.start()
+		prevActions = actions

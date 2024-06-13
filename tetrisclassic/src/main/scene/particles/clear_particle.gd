b61d4455247c9a20801ed638ef2f1f071a06f148
KOFLAzycat <lushoulei0552@163.com>
2024-06-13 12:00:04 +0800

    1. 删除代码

--- tetrisclassic/src/main/scene/particles/clear_particle.gd
+++
@@ -1,9 +0,0 @@
-extends GPUParticles2D
-
-func setBoxRange(rangeX):
-	process_material.emission_box_extents.x = rangeX
-
-func emit():
-	emitting = true
-	await get_tree().create_timer(5.0).timeout
-	queue_free()

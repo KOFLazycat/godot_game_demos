2aeb33c9994324ddb2eed85fa4ff8029e7ef915c
KOFLAzycat <lushoulei0552@163.com>
2024-06-27 12:45:21 +0800

    1. 修改状态机为状态图插件

--- dashdash/src/main/scene/role/player/player_base/player_base.gd
+++ dashdash/src/main/scene/role/player/player_base/player_base.gd
@@ -8,8 +8,8 @@ class_name PlayerBase # Player基类
 @onready var dash_ghost_scene: PackedScene = preload("res://src/main/scene/role/player/dash_ghost/dash_ghost.tscn")
 @onready var health_system: HealthSystem = $HealthSystem
 @onready var hurt_system: HurtSystem = $HurtSystem
-@onready var fsm: Fsm = $Fsm
 @onready var input_manager: InputManager = InputManager.new()
+@onready var state_chart: StateChart = $StateChart
 
 # 加速度
 var accel: float = 2000.0
@@ -31,16 +31,15 @@ func _ready() -> void:
 	hurt_system.hurt.connect(_on_hurt_system_hurt)
 	health_system.health_update.connect(_on_health_system_health_update)
 	health_system.die.connect(_on_health_system_die)
-	fsm.change_state("player_idle_state")
 
 
 func _process(delta: float) -> void:
-	fsm.update(delta)
+	pass
 
 
 func _physics_process(delta: float) -> void:
 	input_manager.update(delta)
-	fsm.physics_update(delta)
+	pass
 
 
 # dash的影子
@@ -72,3 +71,63 @@ func _on_health_system_health_update(health: float, is_heal: bool) -> void:
 
 func _on_health_system_die() -> void:
 	player_die()
+
+
+func _on_idle_state_entered() -> void:
+	animated_sprite_2d.play("idle")
+	run_dust_gpu_particles_2d.emitting = false
+
+
+func _on_idle_state_physics_processing(delta: float) -> void:
+	velocity = velocity.move_toward(Vector2.ZERO, accel * delta)
+	move_and_slide()
+	if input_manager.is_dash:
+		state_chart.send_event("PlayerMoveStateToDash")
+	else: 
+		if input_manager.normalized != Vector2.ZERO:
+			state_chart.send_event("PlayerMoveStateToRun")
+		else: 
+			pass
+
+
+func _on_run_state_entered() -> void:
+	animated_sprite_2d.play("run")
+	#AudioSystem.play_sfx(AudioSystem.SFX_INDEX.RUN)
+	run_dust_gpu_particles_2d.emitting = true
+
+
+func _on_run_state_physics_processing(delta: float) -> void:
+	velocity = velocity.move_toward(speed * input_manager.normalized, accel * delta)
+	facing = input_manager.raw
+	move_and_slide()
+	if input_manager.is_dash:
+		state_chart.send_event("PlayerMoveStateToDash")
+	else: 
+		if input_manager.raw == Vector2.ZERO:
+			state_chart.send_event("PlayerMoveStateToIdle")
+		else: 
+			pass
+
+
+func _on_dash_state_entered() -> void:
+	input_manager.is_dash = true
+	dash_gpu_particles_2d.emitting = true
+	animated_sprite_2d.play("dash")
+	AudioSystem.play_sfx(AudioSystem.SFX_INDEX.DASH)
+	await get_tree().create_timer(0.2, true, false, true).timeout
+	input_manager.is_dash = false
+	dash_gpu_particles_2d.emitting = false
+
+
+func _on_dash_state_physics_processing(delta: float) -> void:
+	if input_manager.is_dash:
+		velocity = velocity.move_toward(dash_speed * input_manager.normalized, accel * delta)
+		facing = input_manager.raw
+		show_dash()
+	move_and_slide()
+	
+	if !input_manager.is_dash:
+		if input_manager.raw == Vector2.ZERO:
+			state_chart.send_event("PlayerMoveStateToIdle")
+		elif input_manager.normalized != Vector2.ZERO:
+			state_chart.send_event("PlayerMoveStateToRun")
